cccc4b63b5b3085d027ece9293746afd3f1bfd86
woaichimangguo <807117433@qq.com>
2018-03-24 17:26:40 +0800

    hello

---
+++ src/KS/STUDENT.java
@@ -0,0 +1,15 @@
+package KS;
+import java.util.Random;
+public class STUDENT {
+  public static void main(String[] args) {
+    String[] operate=new String[]{"+","-","¡Á","¡Â"};
+      int[] numbers=new int[100];
+      for(int i=1;i<=100;i++){
+      numbers[i-1]=i;
+    }
+     Random r=new Random();
+     for(int i=0;i<30;i++){
+       System.out.println(numbers[r.nextInt(100)]+operate[r.nextInt(4)]+numbers[r.nextInt(100)]+operate[r.nextInt(4)]+numbers[r.nextInt(100)]+"=");
+    }
+   }
+   }
